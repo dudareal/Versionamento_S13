e718f9aec3af218dc2d6c6acfda7f80c7c3f9872
Eduarda Real <dudarealferreira07@gmail.com>
2024-06-12 18:41:02 -0300

    correção

--- test_pessoa.py
+++ test_pessoa.py
@@ -29,7 +29,9 @@ class TestePessoa(unittest.TestCase):
 
         pessoa_teste = Pessoa(nome, sobrenome, idade)
 
-        self.assertTrue(False)
+        self.assertTrue(
+            pessoa_teste.get_nome() == f'{nome} {sobrenome}'
+        )
 
     # Teste pegar idade
     def test_get_idade(self):
